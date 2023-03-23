481bd4b8faf982aa1d4d5e9d1e32c62569da7f8b
Jacob Damasco <damasco@usc.edu>
2023-03-23 14:04:46 -0700

    added test 2 for light sensor

--- test_suite.py
+++ test_suite.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import Adafruit_MCP3008
 import time
 
 def blinkLED(pin, delay):
@@ -7,9 +8,24 @@ def blinkLED(pin, delay):
     GPIO.output(pin, GPIO.LOW)
     time.sleep(delay)
 
+def readLightSensor(mcp):
+    sensorVal = mcp.read_adc(0)
+    if (sensorVal > 10):
+        print(sensorVal, ", bright")
+    else:
+        print(sensorVal, ", dark")
+    # delay for 100 ms
+    time.sleep(.1)
+
+
 def main():
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(11, GPIO.OUT)
+    CLK = 18
+    MISO = 23
+    MOSI = 24
+    CS = 25
+    mcp = Adafruit_MCP3008.MCP3008(clk=CLK, cs=CS, miso=MISO, mosi=MOSI)
 
     while True:
         # Test 1: blink LED 5 times w on/off intervals 500ms
@@ -18,6 +34,9 @@ def main():
 
         # Test 2: read output of light sensor w intervals 100 ms
         # print raw value & "light" or "dark" w threshold 10
+        # so run for 50 times in a for loop
+        for i in range(50):
+            readLightSensor(mcp)
 
 
         # Test 3: blink LED 4 times w on/off intervals 200 ms
