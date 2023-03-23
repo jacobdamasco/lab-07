36e66a857814455f0a61bea4e7493509a25c8b45
Jacob Damasco <damasco@usc.edu>
2023-03-23 14:56:39 -0700

    solo sound sensor tests

--- test_suite.py
+++ test_suite.py
@@ -9,13 +9,26 @@ def blinkLED(pin, delay):
     GPIO.output(pin, GPIO.LOW)
     time.sleep(delay)
 
-def readLightSensor(mcp):
-    sensorVal = mcp.read_adc(0)
-    if (sensorVal > 10):
-        print(sensorVal, ", bright")
+def readLightSensor(mcp, channel):
+    sensorVal = mcp.read_adc(channel)
+    if (sensorVal > 130):
+        print(sensorVal, " = bright")
     else:
-        print(sensorVal, ", dark")
+        print(sensorVal, " = dark")
+
     # delay for 100 ms
+    time.sleep(.1)
+
+def readSoundSensor(mcp, channel):
+    sensorVal = mcp.read_adc(channel)
+    if (sensorVal > 100):
+        print(sensorVal)
+    else:
+        print(sensorVal)
+        GPIO.output(11, GPIO.HIGH)
+        time.sleep(.1)
+        GPIO.output(11, GPIO.LOW)
+        
     time.sleep(.1)
 
 
@@ -27,24 +40,26 @@ def main():
     mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
     while True:
-        # Test 1: blink LED 5 times w on/off intervals 500ms
-        for i in range(5):
-            blinkLED(11, 0.5)
+        # # Test 1: blink LED 5 times w on/off intervals 500ms
+        # for i in range(5):
+        #     blinkLED(11, 0.5)
 
-        # Test 2: read output of light sensor w intervals 100 ms
-        # print raw value & "light" or "dark" w threshold 10
-        # so run for 50 times in a for loop
-        for i in range(50):
-            readLightSensor(mcp)
+        # # Test 2: read output of light sensor w intervals 100 ms
+        # # print raw value & "light" or "dark" w threshold 10
+        # # so run for 50 times in a for loop
+        # for i in range(50):
+        #     readLightSensor(mcp, 0)
 
 
-        # Test 3: blink LED 4 times w on/off intervals 200 ms
-        for i in range(4):
-            blinkLED(11, 0.2)
+        # # Test 3: blink LED 4 times w on/off intervals 200 ms
+        # for i in range(4):
+        #     blinkLED(11, 0.2)
 
         # Test 4: read output of sound sensor w intervals 100 ms
         # print raw value 
         # if tapped, LED turns on for 100 ms
+        for i in range(50):
+            readSoundSensor(mcp, 1)
             
 
 if __name__ == "__main__":
